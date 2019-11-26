c4ce72410eb28519c45c1527febebe12cd10d6ef
Adam Nobs <Nobsadam@gmail.com>
2019-11-26 16:06:31 -0600

    Working on ui now and making a menu for the store to tie into the gamerun function in game class

--- Game.cs
+++ Game.cs
@@ -8,6 +8,7 @@ namespace LemonadeStand_3DayStarter
 {
     public class Game
     {
+        private Store store;
         private Player player;
         private List<Day> days = new List<Day>() {};
         private int currentDay;
@@ -15,11 +16,15 @@ namespace LemonadeStand_3DayStarter
         public Game()
         {
             player = new Player();
+            store = new Store();
         }
 
         public void RunGame()
         {
             UserInterface.GameInstructions();
+            UserInterface.DisplayInventory(player.inventory);
+            store.SellLemons(player);
+            
             
         }
 
--- UserInterface.cs
+++ UserInterface.cs
@@ -34,7 +34,7 @@ namespace LemonadeStand_3DayStarter
         public static void GameInstructions()
         {
 
-            Console.WriteLine("Welcome to your lemonade stand!\nYou will have 7 days to make as much profit possible!\nYou'll have complete control over you buisness, including pricing, quality control, inventory control, and purchasing supplies.\nBuy you ingredients, set your recipe, and start selling!");
+            Console.WriteLine("Welcome to your lemonade stand!\nYou will have 7 days to make as much profit possible!\nYou'll have complete control over you buisness, including pricing, quality control, inventory control, and purchasing supplies.\nBuy your ingredients, set your recipe, and start selling!");
             Console.WriteLine("First thing you'll have to worry about is your recipe, make sure you buy enough of all your ingredients, or you won't be able to sell.");
             Console.WriteLine("You'll also have to deal with the weather, which will play a big part when customers are deciding whether or not to buy your lemonade, so make sure to read the weather report every day!");
             Console.WriteLine("When the temperature drops, or the weather turns bad (overcast, cloudy, rain), don't expect them to buy nearly as much as the would on a hot, hazy day, so buy accoringly\nFeel free to set your prices higher on those hot, muggy days too, as you'll make more profit, even if you sell a bit less lemonade.");
@@ -69,6 +69,29 @@ namespace LemonadeStand_3DayStarter
             Console.WriteLine("You currently have " + inventory.iceCubes.Count + " icecubes");
             Console.WriteLine("You currently have " + inventory.cups.Count + " cups");
             
+        }
+
+        public static void StoreMenu()
+        {
+            Console.WriteLine("Welcome to the good ole store what would you like to purchase?\nPlease enter one of the following you would like to purchase.\nLemons/Sugar/IceCubes/Cups or quit to exit game.");
+            string storeRef = Console.ReadLine().ToLower().Trim();
+            switch (storeRef)
+            {
+                case "lemon":
+                    break;
+                case "sugar":
+                    break;
+                case "icecubes":
+                    break;
+                case "cups":
+                    break;
+                case "quit":
+                    break;
+                default:
+                    Console.WriteLine("Please try again and enter a valid item");
+                    StoreMenu();
+                    break;
+            }
         }
 
 
